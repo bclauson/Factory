3eae9a4f2d2c4a0884095e7663550bdd73487f4b
bclauson <clauson.branden@gmail.com>
2022-03-26 21:50:39 -0700

    add delete engineer in detials section

--- Factory/Controllers/EngineersController.cs
+++ Factory/Controllers/EngineersController.cs
@@ -75,16 +75,31 @@ namespace Factory.Controllers
         return View(thisEngineer);
     }
 
-  [HttpPost]
-  public ActionResult AddMachine(Engineer engineer, int MachineId)
-  {
-      if (MachineId != 0)
-      {
-        _db.MachineEngineer.Add(new MachineEngineer() { MachineId = MachineId, EngineerId = engineer.EngineerId });
+    [HttpPost]
+    public ActionResult AddMachine(Engineer engineer, int MachineId)
+    {
+        if (MachineId != 0)
+        {
+          _db.MachineEngineer.Add(new MachineEngineer() { MachineId = MachineId, EngineerId = engineer.EngineerId });
+          _db.SaveChanges();
+        }
+        return RedirectToAction("Index");
+    }
+
+    public ActionResult Delete(int id)
+    {
+        var thisEngineer = _db.Engineers.FirstOrDefault(engineer => engineer.EngineerId == id);
+        return View(thisEngineer);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+        var thisEngineer = _db.Engineers.FirstOrDefault(engineer => engineer.EngineerId == id);
+        _db.Engineers.Remove(thisEngineer);
         _db.SaveChanges();
-      }
-      return RedirectToAction("Index");
-  }
+        return RedirectToAction("Index");
+    }
 
 
   }
