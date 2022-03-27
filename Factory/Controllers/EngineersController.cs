b771b7c1dd60886476e9b6fa1859ea7924dce600
bclauson <clauson.branden@gmail.com>
2022-03-26 20:54:12 -0700

    add engineer controller and views for index and details

---
+++ Factory/Controllers/EngineersController.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Factory.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Factory.Controllers
+{
+  public class EngineersController : Controller
+  {
+    private readonly FactoryContext _db;
+
+    public EngineersController(FactoryContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+        return View(_db.Engineers.ToList());
+    }
+
+
+    public ActionResult Details(int id)
+    {
+    var thisEngineer = _db.Engineers
+        .Include(Engineer => Engineer.JoinEntities)
+        .ThenInclude(join => join.Machine)
+        .FirstOrDefault(Engineer => Engineer.EngineerId == id);
+    return View(thisEngineer);
+    }
+
+  }
+
+}
